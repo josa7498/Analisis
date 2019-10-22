d61ed6631b565c519334e2c85686b74ee38086c4
Foster2594 <55931613+Foster2594@users.noreply.github.com>
2019-10-21 18:44:51 -0600

    Inicio
    inicio

---
+++ Proyecto.UI/Controllers/Mantenimiento/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.UI.Controllers.Mantenimiento
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+
+
+
+            return View();
+        }
+    }
+}
